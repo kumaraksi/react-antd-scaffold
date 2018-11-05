bb71265d9a340489e8afe476aa0bcde6e4264917
Akshay <kumaraksi@outlook.com>
2018-11-05 08:49:05 +0530

    bootstrap redux

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
 import RouteManager from './RouteManager';
+import Reducers from './store/reducers'
+
 import './app.scss';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className='App'>
-				<RouteManager/>
-			</div>
+			<Provider store={ createStoreWithMiddleware(Reducers) } >
+				<div className='App'>
+					<RouteManager/>
+				</div>
+			</Provider>
 		);
 	}
 }
